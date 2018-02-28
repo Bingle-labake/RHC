8c084a260408bf97ce7c59910672751427660703
yangaibing <yangaibing@qq.com>
2018-02-28 22:50:34 +0800

    first commit

---
+++ app/src/main/java/com/example/wanghy/restauranthygienechecker/LocationSearchFragment.java
@@ -0,0 +1,23 @@
+package com.example.wanghy.restauranthygienechecker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by wanghy on 2018/2/26.
+ */
+
+public class LocationSearchFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.location_search,null);
+        return myView;
+    }
+}
